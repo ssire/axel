ffc6b4f6a29e28c86524ba84d6859f7b2d36a582
Sire <s.sire@free.fr>
2012-09-08 23:08:48 +0400

    Finished templates folder reorganization to be more documentation-oriented; added a few templates, still a few more to add, goal is to have a template file for each interesting enough plugin / filter, note that these template files can also serve to test code during development (some of which can also be doubled with an auto-test in the test folder too).

---
+++ templates/shared/demo.js
@@ -0,0 +1,36 @@
+$(
+  // Extracts xt:attribute and xt:use from <div class="demo"> fragments
+  // Generates and insert the source code before the fragment
+  // NOTE: MUST be called before transforming the template !
+  function sourceDemo () {
+    $('div.extract').each( 
+      function(index, el) { 
+        var buffer = [];
+        var items = this.getElementsByTagNameNS("*", "*");
+        $.each(items,
+          function (index, el) {
+            var accu;
+            var n = el.nodeName.toLowerCase();
+            if ((n === 'xt:use') || (n === 'xt:attribute')) {
+              if (n.charAt(2) == ':') {
+                n = n.substr(3);
+              }
+              accu = [];
+              $.each(el.attributes, 
+                    function(index, attr) {
+                      accu.push(attr.nodeName + "='" + attr.nodeValue + "'"); 
+                    });
+              if (el.textContent) {
+                buffer.push('&lt;xt:' + n + ' ' + accu.join(' ') + '>' + el.textContent + '&lt;/xt:' + n + '>');
+              } else {
+                buffer.push('&lt;xt:' + n + ' ' + accu.join(' ') + '/>');
+              }
+            }
+          }
+        );
+        var src = "<div class='source'><pre>" + buffer.join('<br/>') + "</pre></div>";
+        $(this).before(src); 
+      }
+    ); 
+  }
+);
