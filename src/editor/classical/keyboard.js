162dc390588f0c3cf4119425856f10b8b37b8dbd
Sire <s.sire@oppidoc.fr>
2014-02-25 00:28:31 +0100

    New tab manager algorithm.

--- src/editor/classical/keyboard.js
+++ src/editor/classical/keyboard.js
@@ -2,14 +2,14 @@
  *
  * @COPYRIGHT@
  *
- * This file is part of the Adaptable XML Editing Library (AXEL), version @VERSION@ 
+ * This file is part of the Adaptable XML Editing Library (AXEL), version @VERSION@
  *
  * @LICENSE@
  *
  * Web site : http://media.epfl.ch/Templates/
- * 
+ *
  * Author(s) : Stephane Sire, Antoine Yersin
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 
 // open issue: should we create one keyboard instance per document or not ?
@@ -17,47 +17,32 @@
 /**
  * Centralizes keyboard input among several input devices (e.g. each <input> or <textarea>)
  * This is useful in particular to factorize the tab management code
- * 
- * @class Keyboard
  */
 xtiger.editor.Keyboard = function () {
   this.tabGroupManager = false;
-  this.currentDevice = false;   
+  this.currentDevice = false;
   this.allowRC = false;
 }
 
 // FIXME: dans register memoriser tous les abonnements pour les desabonner sur une method (reset)
 // à appeler quand on change de document // frame (?)
 xtiger.editor.Keyboard.prototype = {
-    
-    /**
-     * 
-     * @param t
-     * @return
-     */
+
   setTabGroupManager : function (t) {
     this.tabGroupManager = t;
   },
-  
-  /**
-   * Allows a device to register to keyboard events on its handle.
-   * 
-   * @param {Device}
-   *            aDevice
-   * @param {DOMElement|DOMDocument}
-   *            aAltHandle Optional. A handle listening to DOM events. If not
-   *            submitted, the device's default handle (device.getHandle()) is
-   *            used.
-   * @return {[function]} Handler's references to unregister
-   */
+
+  // Allows device aDevice to register to keyboard events on its handle
+  // The optional aAltHandle will be used to listen to DOM keyboard events
+  // otherwise it will be the device's default handle (device.getHandle())
+  // Returns an handlers object to be recalled for unregister
   register : function (aDevice, aAltHandle) {
-    
     var _handle = aAltHandle ? aAltHandle : aDevice.getHandle();
-    
+
     // closure variables
     var _this = this;
     var _device = aDevice;
-    
+
     var _handlers = {
         keydown: function (ev) { _this.handleKeyDown(ev, _device) },
         keyup: function (ev) { _this.handleKeyUp(ev, _device) }
@@ -66,61 +51,61 @@ xtiger.editor.Keyboard.prototype = {
     xtdom.addEventListener(_handle, 'keyup', _handlers.keyup, false);
     return _handlers;
   },
-  
-  /**
-   * 
-   * @param device
-   * @param handlers
-   * @return
-   */
-  unregister : function (aDevice, handlers, aAltHandle) {     
+
+  unregister : function (aDevice, handlers, aAltHandle) {
     var _handle = aAltHandle ? aAltHandle : aDevice.getHandle();
     xtdom.removeEventListener(_handle, 'keydown', handlers.keydown, false);
     xtdom.removeEventListener(_handle, 'keyup', handlers.keyup, false);
   },
-  
+
   // Esc does not trigger keyPress on Safari, hence we need to intercept it with keyDown
   handleKeyDown : function (ev, device) {
     if (device.isEditing()) {
-      if (this.tabGroupManager) {
-        this.tabGroupManager.filterKeyDown(ev);
-      }                              
-      // On FF ctrlKey+ RC sends an event but the line break is not added to the textarea hence I have selected shiftKey                       
+      if (this.tabGroupManager && this.tabGroupManager.filterKeyDown(ev)) {
+        return false;
+      }
+      // On FF ctrlKey+ RC sends an event but the line break is not added to the textarea hence I have selected shiftKey
       var validate = (this.allowRC && (ev.keyCode == 13) && (! ev.shiftKey)) || ((!this.allowRC) && (ev.keyCode == 13));
       if (validate) {
         device.stopEditing(false);
         xtdom.preventDefault(ev); /* avoid triggering buttons in IE (e.g. Save button) */
       } else if (ev.keyCode == 27) {
         device.cancelEditing ();
-      }     
-      device.doKeyDown (ev);      
-    } 
+      }
+      device.doKeyDown (ev);
+    }
+    return true;
   },
+
   handleKeyUp : function (ev, device) {
     if (device.isEditing()) {
-      if (this.tabGroupManager && this.tabGroupManager.filterKeyPress(ev)) {      
+      if (this.tabGroupManager && this.tabGroupManager.filterKeyPress(ev)) {
         xtdom.preventDefault(ev);
         xtdom.stopPropagation(ev);
+        return false;
       } else {
         device.doKeyUp (ev);
       }
     }
+    return true;
   },
+
   grab : function (device, editor) {
     if (this.tabGroupManager) {
       this.tabGroupManager.startEditingSession (editor);
-      // window.console.log('TabGroupManager start editing ' + editor);
     }
   },
+
   release : function (device, editor) {
     if (this.tabGroupManager) {
       this.tabGroupManager.stopEditingSession ();
-      // window.console.log('TabGroupManager stop editing');
     }
   },
-  enableRC : function () {  
-    this.allowRC = true;     
-  }, 
+
+  enableRC : function () {
+    this.allowRC = true;
+  },
+
   disableRC : function () {
     this.allowRC = false;
   }
--- src/editor/classical/tabgroupmgr.js
+++ src/editor/classical/tabgroupmgr.js
@@ -2,14 +2,14 @@
  *
  * @COPYRIGHT@
  *
- * This file is part of the Adaptable XML Editing Library (AXEL), version @VERSION@ 
+ * This file is part of the Adaptable XML Editing Library (AXEL), version @VERSION@
  *
  * @LICENSE@
  *
  * Web site : http://media.epfl.ch/Templates/
- * 
+ *
  * Author(s) : Stephane Sire
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 
 /*
@@ -17,93 +17,207 @@
  */
 xtiger.editor.TabGroupManager = function (rootNode) {
   this.root = rootNode;
-  this.isChangingFocus = false;   
   this.direction = 0;
 }
 
 xtiger.editor.TabGroupManager.prototype = {
-  
+
   startEditingSession : function (editor) {
-    if (this.isChangingFocus) return; // guard
-    this.tabs = [];   
-    var treeWalker = xtiger.cross.makeTreeWalker (this.root, xtdom.NodeFilter.SHOW_ELEMENT,
-        function (node) { 
-            if (node.xttPrimitiveEditor && node.xttPrimitiveEditor.isFocusable()) {
-              return xtdom.NodeFilter.FILTER_ACCEPT
-            } else {
-              return xtdom.NodeFilter.FILTER_SKIP; 
-            }
-        } );
-    while(treeWalker.nextNode()) {
-      // FIXME: how to avoid input nodes within unselected XttChoiceEditor control ?
-      this.tabs.push(treeWalker.currentNode.xttPrimitiveEditor);    
-    }   
     this.curEditor = editor;
   },
 
   stopEditingSession : function () {
-    if (this.isChangingFocus) return; // guard
-    this.tabs = undefined;
     this.curEditor = undefined;
   },
-  
+
   // Intercepts Tab KeyDown events
   // Returns true if it has intercepted it
-  filterKeyDown : function (ev) {       
+  filterKeyDown : function (ev) {
     this.direction = 0; // For firefox
     if (ev.keyCode == 9) { // it's a Tab
         if (xtiger.cross.UA.gecko)  { // we must wait for KeyPress event because canceling will not work
-          this.direction = ev.shiftKey ? -1 : 1;          
+          this.direction = ev.shiftKey ? -1 : 1;
         } else { // we can act immediatly
-          this._focusNextInput(ev.shiftKey ? -1 : 1);
-        }               
+          if (!this._focusNextInput(ev.shiftKey ? -1 : 1)) {
+            return false;
+          }
+        }
         try {
           xtdom.preventDefault(ev);
           xtdom.stopPropagation(ev);
         } catch (e) {
-          // on IE !          
+          // on IE !
         }
         return true
     } else {
       return false;
     }
-  },      
-  
+  },
+
   // For some certain kinds of browsers filterKeyDown does not cancel the Tab event
   // in that case we get a chance to modify its effect in KeyPress event handling
   // This is the case with Firefox (v2 on OS X at least)
   filterKeyPress : function (ev) {
     if (xtiger.cross.UA.gecko && (this.direction != 0)) {
-      // window.console.log('Focused next input');
-      return (this._focusNextInput(this.direction));
+      if (ev.keyCode === 9) {
+        var res = this._focusNextInput(this.direction);
+        return res;
+      }
     }
     return false;
-  },  
-  
+  },
+
   _focusNextInput : function (direction) {
-    var res = false;
-    if (!this.tabs)
-      return; // safety guard
-    for (var i = 0; i < this.tabs.length; i++) {
-      if (this.tabs[i] == this.curEditor) {
-        break;
+    var start = this.curEditor.getHandle(true);
+    if (start) {
+      if (direction > 0) {
+        return this._focusNextInputIter(start);
+      } else if (direction < 0) {
+        return this._focusPrevInputIter(start);
       }
     }
-    if (i < this.tabs.length) {
-      var next;
-      if ((i + direction) < 0) {
-        next = this.tabs.length - 1;
+    return false;
+  },
+
+  _focusNextInputIter : function (node) {
+    var p = node.parentNode,
+        last = p.lastChild,
+        res;
+    if (!p || (p.tagName === 'html') || p.xttHeadLabel) { // reached top
+      return false;
+    } else {
+      if (node.nextSibling) {
+        if (node.nextSibling === last) {
+          res = this._nodeIter(last);
+        } else {
+          res = this._fwdSliceIter(node.nextSibling);
+        }
+        return res || this._focusNextInputIter(p);
       } else {
-        next = (i + direction) % this.tabs.length;
+        return this._focusNextInputIter(p);
       }
-      this.isChangingFocus = true;  
-      this.tabs[i].unfocus();       
-      this.tabs[next].focus ();
-      this.isChangingFocus = false;
-      this.curEditor = this.tabs[next];
-      res = true;
-    }        
-    this.direction = 0;
-    return res;
-  } 
-} 
+    }
+  },
+
+  _focusPrevInputIter : function (node) {
+    var p = node.parentNode,
+        first = p.firstChild,
+        res;
+    if (!p || (p.tagName === 'html') || p.xttHeadLabel) { // reached top
+      return false;
+    } else {
+      if (node.previousSibling) {
+        if (node.previousSibling === first) {
+          res = this._antiNodeIter(first);
+        } else {
+          res = this._antiSliceIter(node.previousSibling);
+        }
+        return res || this._focusPrevInputIter(p);
+      } else {
+        return this._focusPrevInputIter(p);
+      }
+    }
+  },
+
+  _nodeIter : function (n) {
+    if (n.xttHeadLabel) { // avoids nested editors
+      return false;
+    }
+    if (n.xttPrimitiveEditor && n.xttPrimitiveEditor.isFocusable()) {
+      this.curEditor.unfocus();
+      n.xttPrimitiveEditor.focus ();
+      this.curEditor = n.xttPrimitiveEditor;
+      return true;
+    }
+    if (n.firstChild) {
+      return this._fwdSliceIter(n.firstChild);
+    }
+    return false;
+  },
+
+  // origin is optional, it is the Choice editor from where a recursive call has been initiated
+  _fwdSliceIter : function (begin, origin) {
+    var cur = begin,
+        found = false,
+        c;
+    while (cur && (!found)) {
+      if (cur.startRepeatedItem) {
+        if (cur.startRepeatedItem.getSize() === 0) { // jumps other unselected repeater (min=0)
+          cur = cur.startRepeatedItem.getLastNodeForSlice(0);
+          cur = cur.nextSibling;
+          continue;
+        }
+      }
+      if (cur.beginChoiceItem && (cur.beginChoiceItem != origin)) {
+        c = cur.beginChoiceItem; // checks current choice slice
+        if (c.items[c.curItem][0] !== c.items[c.curItem][1]) {
+          found = this._fwdSliceIter(c.items[c.curItem][0], c);
+        } else {
+          // a choice slice starts and end on the same node
+          found = this._nodeIter(c.items[c.curItem][0]);
+        }
+        cur = c.items[c.items.length - 1][1]; // sets cur to the last choice
+      } else if (cur.endChoiceItem && (cur.endChoiceItem != origin)) {
+        c = cur.endChoiceItem; // no need to check other choice item since we started in the middle
+        cur = c.items[c.items.length - 1][1]; // sets cur to the last choice
+      } else {
+        found = this._nodeIter(cur);
+      }
+      if (! found) {
+        cur = cur.nextSibling;
+      }
+    }
+    return found;
+  },
+
+  _antiNodeIter : function (n) {
+    if (n.xttHeadLabel) { // avoids nested editors
+      return false;
+    }
+    if (n.xttPrimitiveEditor && n.xttPrimitiveEditor.isFocusable()) {
+      this.curEditor.unfocus();
+      n.xttPrimitiveEditor.focus ();
+      this.curEditor = n.xttPrimitiveEditor;
+      return true;
+    }
+    if (n.firstChild) {
+      return this._antiSliceIter(n.lastChild);
+    }
+    return false;
+  },
+
+	// origin is optional, it is the Choice editor from where a recursive call has been initiated
+  _antiSliceIter : function (end, origin) {
+    var cur = end,
+        found = false,
+        c;
+    while (cur && (!found)) {
+      if (cur.endRepeatedItem) {
+        if (cur.endRepeatedItem.getSize() === 0) {  // jumps other unselected repeater (min=0)
+          cur = cur.endRepeatedItem.getFirstNodeForSlice(0);
+          cur = cur.previousSibling;
+          continue;
+        }
+      }
+      if (cur.endChoiceItem && (cur.endChoiceItem != origin)) {
+        c = cur.endChoiceItem;
+        if (c.items[c.curItem][0] !== c.items[c.curItem][1]) {
+          found = this._antiSliceIter(c.items[c.curItem][1], c);
+        } else {
+          // a choice slice starts and end on the same node
+          found = this._antiNodeIter(c.items[c.curItem][0]);
+        }
+        cur = c.items[0][0]; // sets cur to the last choice
+      } else if (cur.beginChoiceItem && (cur.beginChoiceItem != origin)) {
+        c = cur.beginChoiceItem; // no need to check other choice item since we started in the middle
+        cur = c.items[0][0]; // sets cur to the first choice
+      } else {
+        found = this._antiNodeIter(cur); // FIXME:  first interpretation
+      }
+      if (! found) {
+        cur = cur.previousSibling;
+      }
+    }
+    return found;
+  }
+}
